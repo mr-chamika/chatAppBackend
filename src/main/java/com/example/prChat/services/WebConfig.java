50e5fba2917edc7118f02b772fdab45e00fcea6e
mr-chamika <chamikauni2001@gmail.com>
2025-08-30 11:06:35 +0530

    changed true to false

--- src/main/java/com/example/prChat/services/WebConfig.java
+++ src/main/java/com/example/prChat/services/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 }
