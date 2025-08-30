2a8a99820efc861b4174fc73a0d490a71d100141
mr-chamika <chamikauni2001@gmail.com>
2025-08-30 10:50:40 +0530

    fix the correct backend

--- src/main/java/com/example/prChat/services/WebConfig.java
+++ src/main/java/com/example/prChat/services/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081","http://localhost:5173")
+                .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
